6e39fcae13f20446fce5d9ef8d2aad92626a17e1
(no author) <(no author)@87cc427b-4a1e-0410-b729-b2260ec0f063>
2005-11-28 19:06:54 +0000

    This commit was manufactured by cvs2svn to create tag 'virtuemart-1_0_1-released'.
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@167 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ tags/virtuemart-1_0_1-released/virtuemart/html/shop.cart.php
@@ -0,0 +1,64 @@
+<?php
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' ); 
+/**
+*
+* @version $Id: shop.cart.php,v 1.2 2005/09/27 17:51:26 soeren_nb Exp $
+* @package VirtueMart
+* @subpackage html
+* @copyright Copyright (C) 2004-2005 Soeren Eberhardt. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* VirtueMart is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See /administrator/components/com_virtuemart/COPYRIGHT.php for copyright notices and details.
+*
+* http://virtuemart.net
+*/
+mm_showMyFileName( __FILE__ );
+
+$mainframe->setPageTitle( $VM_LANG->_PHPSHOP_CART_TITLE );
+
+$show_basket = true;
+
+?>
+<h2><?php echo $VM_LANG->_PHPSHOP_CART_TITLE ?></h2>
+<!-- Cart Begins here -->
+<?php include(PAGEPATH. 'basket.php'); ?>
+<!-- End Cart -->
+<?php
+if ($cart["idx"]) {
+ ?>
+ <br />
+ <div style="text-align:center;width:40%;float:left;">
+     <h3><a href="<?php echo $_SERVER['HTTP_REFERER'] ?>">
+     <img src="<?php echo IMAGEURL ?>ps_image/back.png" align="middle" width="32" height="32" alt="Back" border="0" />
+      <?php echo $VM_LANG->_PHPSHOP_CONTINUE_SHOPPING; ?>
+     </a></h3>
+ </div>
+ <?php
+   if (!defined('_MIN_POV_REACHED')) { ?>
+       <div style="text-align:center;width:40%;float:left;">
+       <br /><br />
+           <span style="font-weight:bold;"><?php echo $VM_LANG->_PHPSHOP_CHECKOUT_ERR_MIN_POV2 . " ".$CURRENCY_DISPLAY->getFullValue($_SESSION['minimum_pov']) ?></span>
+       </div><?php
+   }
+   else {
+ ?>
+ <div style="text-align:center;width:40%;float:left;">
+     <h3><a href="<?php $sess->purl( $mm_action_url . "index.php?page=checkout.index&ssl_redirect=1"); ?>">
+     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="<?php echo IMAGEURL ?>ps_image/forward.png" align="middle" width="32" height="32" alt="Forward" border="0" />
+      <?php echo $VM_LANG->_PHPSHOP_CHECKOUT_TITLE ?>
+     </a></h3>
+ </div>
+ 
+ <?php
+ }
+ ?>
+<br style="clear:both;" /><br/>
+
+<?php
+// End if statement
+}
+?>
+

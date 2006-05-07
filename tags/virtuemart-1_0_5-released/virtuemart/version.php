d4b284fdc00d963aecfe9f97e82f3ec0220a2b3a
(no author) <(no author)@87cc427b-4a1e-0410-b729-b2260ec0f063>
2006-05-07 12:39:03 +0000

    This commit was manufactured by cvs2svn to create tag 'virtuemart-1_0_5-released'.
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@306 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ tags/virtuemart-1_0_5-released/virtuemart/version.php
@@ -0,0 +1,59 @@
+<?php 
+defined( '_VALID_MOS' ) or die( 'Direct Access to this location is not allowed.' ); 
+/**
+*
+* @version $Id: version.php,v 1.15.2.8 2006/03/28 19:40:14 soeren_nb Exp $
+* @package VirtueMart
+* @subpackage core
+* @copyright Copyright (C) 2004-2005 Soeren Eberhardt. All rights reserved.
+* @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+* VirtueMart is free software. This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See /administrator/components/com_virtuemart/COPYRIGHT.php for copyright notices and details.
+*
+* http://virtuemart.net
+*/
+if( class_exists( 'vmVersion' ) ) {
+	$VMVERSION =& new vmVersion();
+	
+	$shortversion = $VMVERSION->PRODUCT . " " . $VMVERSION->RELEASE . " " . $VMVERSION->DEV_STATUS. " ";
+		
+	$myVersion = $shortversion . " [".$VMVERSION->CODENAME ."] <br />" . $VMVERSION->RELDATE . " "
+	. $VMVERSION->RELTIME . " " . $VMVERSION->RELTZ;
+	return;
+}
+if( !class_exists( 'vmVersion' ) ) {
+/** Version information */
+class vmVersion {
+	/** @var string Product */
+	var $PRODUCT = 'VirtueMart';
+	/** @var int Release Number */
+	var $RELEASE = '1.0.5';
+	/** @var string Development Status */
+	var $DEV_STATUS = 'stable';
+	/** @var string Codename */
+	// Song written by Willie Dixon for Howlin' Wolf; http://en.wikipedia.org/wiki/Willie_Dixon
+	var $CODENAME = 'Built for Comfort';
+	/** @var string Date */
+	var $RELDATE = '07/05/2006';
+	/** @var string Time */
+	var $RELTIME = '19:23';
+	/** @var string Timezone */
+	var $RELTZ = 'GMT';
+	/** @var string Copyright Text */
+	var $COPYRIGHT = 'Copyright (C) 2005-2006 Soeren Eberhardt. All rights reserved.'; 
+	/** @var string URL */
+	var $URL = '<a href="http://virtuemart.net">VirtueMart</a> is a Free Component for Joomla!/Mambo released under the GNU/GPL License.';
+}
+$VMVERSION =& new vmVersion();
+
+$shortversion = $VMVERSION->PRODUCT . " " . $VMVERSION->RELEASE . " " . $VMVERSION->DEV_STATUS. " ";
+	
+$myVersion = $shortversion . " [".$VMVERSION->CODENAME ."] <br />" . $VMVERSION->RELDATE . " "
+	. $VMVERSION->RELTIME . " " . $VMVERSION->RELTZ;
+	
+}
+
+?>

b0a08ae79948268bf533eff202c267aef4aeefe0
joomlacorner <joomlacorner@87cc427b-4a1e-0410-b729-b2260ec0f063>
2009-06-03 13:36:28 +0000

    add PaySbuy Payment gateway
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@1807 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ branches/virtuemart-1.1/virtuemart/classes/payment/ps_paysbuy.cfg.php
@@ -0,0 +1,8 @@
+<?php
+if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' ); ; 
+
+define ('PaySbuy_EMAIL', 'akarawuth@marvelic.co.th');
+define ('PaySbuy_CURRENCY', '840');
+define ('PaySbuy_GATEWAY', '1');
+define ('PaySbuy_LANG', '0');
+?>

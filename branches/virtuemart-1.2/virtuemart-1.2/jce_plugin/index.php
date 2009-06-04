eda5e71d17145f4125e043bb584493a607fc4a45
Daniel Jonsson <Daniel Jonsson@87cc427b-4a1e-0410-b729-b2260ec0f063>
2009-06-04 18:47:32 +0000

    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@1809 87cc427b-4a1e-0410-b729-b2260ec0f063

--- branches/virtuemart-1.2/virtuemart-1.2/jce_plugin/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-defined( '_VALID_MOS' ) or die( 'Direct access to this location is not allowed!' );
-?>
--- branches/virtuemart-1.2/virtuemart-1.2/jce_plugin/langs/en.js
+++
@@ -1,8 +0,0 @@
-// UK lang variables
-
-tinyMCE.addToLang('productsnap',{
-desc : 'Insert/Edit Product Snapshot',
-missing_selection : 'Please select a product from the list.',
-delta_width : 0,
-delta_height : 0
-});

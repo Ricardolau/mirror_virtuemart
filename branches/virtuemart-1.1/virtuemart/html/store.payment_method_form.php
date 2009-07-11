53e2cf618055c9a35f92572c399f223a80bf1250
tkahl <tkahl@87cc427b-4a1e-0410-b729-b2260ec0f063>
2009-07-11 13:17:53 +0000

    Added tax dropdown for payment-discount
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@1849 87cc427b-4a1e-0410-b729-b2260ec0f063

--- branches/virtuemart-1.1/virtuemart/html/store.payment_method_form.php
+++ branches/virtuemart-1.1/virtuemart/html/store.payment_method_form.php
@@ -1,5 +1,5 @@
 <?php
-if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' ); 
+if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' );
 /**
 *
 * @version $Id$
@@ -30,8 +30,8 @@ $default['payment_class'] = 'ps_payment';
 
 if (!empty($payment_method_id)) {
     $q = "SELECT * FROM #__{vm}_payment_method WHERE vendor_id='$ps_vendor_id' AND ";
-    $q .= "payment_method_id='$payment_method_id'"; 
-    $db->query($q);  
+    $q .= "payment_method_id='$payment_method_id'";
+    $db->query($q);
     $db->next_record();
 }
 
@@ -62,15 +62,15 @@ $tabs->startTab( $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_LBL'), "global-page");
       <td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_ISSHIP_LIST_PUBLISH_LBL') ?>?:</td>
       <td><input type="checkbox" name="payment_enabled" class="inputbox" value="Y" <?php echo $db->sf("payment_enabled")=="Y" ? "checked=\"checked\"" : "" ?> /></td>
     </tr>
-    <tr class="row1"> 
+    <tr class="row1">
       <td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_NAME') ?>:</td>
-      <td width="69%" > 
+      <td width="69%" >
         <input type="text" class="inputbox" name="payment_method_name" value="<?php $db->sp("payment_method_name") ?>" size="32" />
       </td>
     </tr>
-    <tr class="row0"> 
+    <tr class="row0">
       <td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_CODE') ?>:</td>
-      <td width="69%" > 
+      <td width="69%" >
         <input type="text" class="inputbox" name="payment_method_code" value="<?php $db->sp("payment_method_code") ?>" size="4" maxlength="8" />
       </td>
     </tr>
@@ -80,20 +80,20 @@ $tabs->startTab( $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_LBL'), "global-page");
           ?>
       </td>
       <td width="69%">
-      	<?php 
+      	<?php
       	echo ps_payment_method::list_available_classes( 'payment_class', $db->sf("payment_class") ? $db->sf("payment_class") : 'ps_payment' );
       	echo mm_ToolTip( $VM_LANG->_('VM_PAYMENT_CLASS_NAME_TIP') ); ?>
       </td>
     </tr>
-    <tr class="row0"> 
+    <tr class="row0">
       <td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_ENABLE_PROCESSOR') ?>:</td>
       <td width="69%" >
-      <?php 
-          $payment_process = $db->f("enable_processor"); 
-          $payment_types = array( "" => $VM_LANG->_('PHPSHOP_PAYMENT_FORM_CC'), 
-                              "Y" => $VM_LANG->_('PHPSHOP_PAYMENT_FORM_USE_PP'), 
-                              "B" => $VM_LANG->_('PHPSHOP_PAYMENT_FORM_BANK_DEBIT'), 
-                              "N" => $VM_LANG->_('PHPSHOP_PAYMENT_FORM_AO'), 
+      <?php
+          $payment_process = $db->f("enable_processor");
+          $payment_types = array( "" => $VM_LANG->_('PHPSHOP_PAYMENT_FORM_CC'),
+                              "Y" => $VM_LANG->_('PHPSHOP_PAYMENT_FORM_USE_PP'),
+                              "B" => $VM_LANG->_('PHPSHOP_PAYMENT_FORM_BANK_DEBIT'),
+                              "N" => $VM_LANG->_('PHPSHOP_PAYMENT_FORM_AO'),
                               "P" => $VM_LANG->_('VM_PAYMENT_FORM_FORMBASED') );
           $i = 0;
           foreach( $payment_types as $value => $description) {
@@ -116,23 +116,23 @@ $tabs->startTab( $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_LBL'), "global-page");
     <tr class="row1">
       <td colspan="2">&nbsp;</td>
     </tr>
-    <tr class="row0"> 
+    <tr class="row0">
       <td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_SHOPPER_GROUP') ?>:</td>
-      <td width="69%" ><?php 
-      		echo ps_shopper_group::list_shopper_groups("shopper_group_id", $db->sf("shopper_group_id")) ?> 
+      <td width="69%" ><?php
+      		echo ps_shopper_group::list_shopper_groups("shopper_group_id", $db->sf("shopper_group_id")) ?>
       </td>
     </tr>
-    <tr class="row1"> 
+    <tr class="row1">
       <td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_DISCOUNT') ?>:</td>
-      <td width="69%" > 
+      <td width="69%" >
         <input type="text" class="inputbox" name="payment_method_discount" size="6"  value="<?php $db->sp("payment_method_discount"); ?>" />
         <?php echo mm_ToolTip( $VM_LANG->_('VM_PAYMENT_METHOD_DISCOUNT_TIP') ); ?>
       </td>
     </tr>
-    
-    <tr class="row1"> 
+
+    <tr class="row1">
       <td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_PRODUCT_DISCOUNT_AMOUNTTYPE') ?>:</td>
-      <td width="69%"> 
+      <td width="69%">
         <input type="radio" class="inputbox" id="payment_method_discount_is_percent0" name="payment_method_discount_is_percent" value="1" <?php if($db->sf("payment_method_discount_is_percent")==1) echo "checked=\"checked\""; ?> />
         <label for="payment_method_discount_is_percent0"><?php echo $VM_LANG->_('PHPSHOP_PRODUCT_DISCOUNT_ISPERCENT') ?></label>&nbsp;&nbsp;&nbsp;
         <?php echo mm_ToolTip( $VM_LANG->_('PHPSHOP_PRODUCT_DISCOUNT_ISPERCENT_TIP') ); ?><br />
@@ -140,39 +140,52 @@ $tabs->startTab( $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_LBL'), "global-page");
         <label for="payment_method_discount_is_percent1"><?php echo $VM_LANG->_('PHPSHOP_PRODUCT_DISCOUNT_ISTOTAL') ?></label>
       </td>
     </tr>
-    <tr class="row1"> 
+
+    <tr class="row1">
+    	<td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_RATE_FORM_VAT_ID') ?>:</td>
+    	<td >
+		<?php
+			require_once(CLASSPATH.'ps_tax.php');
+			$ps_tax = new ps_tax;
+			$ps_tax->list_tax_value("payment_method_discount_vat_id",$db->sf("payment_method_discount_vat_id"));
+			echo mm_ToolTip( $VM_LANG->_('PHPSHOP_PRODUCT_DISCOUNT_VATID_TIP') );
+		?>
+    	</td>
+   	</tr>
+
+    <tr class="row1">
     	<td class="labelcell"><?php echo $VM_LANG->_('VM_PAYMENT_METHOD_DISCOUNT_MAX_AMOUNT') ?>:</td>
     	<td ><input type="text" name="payment_method_discount_max_amount" value="<?php $db->sp('payment_method_discount_max_amount') ?>" size="5" /></td>
    	</tr>
-   	
-    <tr class="row1"> 
+
+    <tr class="row1">
     	<td class="labelcell"><?php echo $VM_LANG->_('VM_PAYMENT_METHOD_DISCOUNT_MIN_AMOUNT') ?>:</td>
     	<td ><input type="text" name="payment_method_discount_min_amount" value="<?php $db->sp('payment_method_discount_min_amount') ?>" size="5" /></td>
    	</tr>
-   	
-    <tr class="row0"> 
+
+    <tr class="row0">
       <td class="labelcell"><?php echo $VM_LANG->_('PHPSHOP_PAYMENT_METHOD_FORM_LIST_ORDER') ?>:</td>
-      <td width="69%" > 
+      <td width="69%" >
         <input type="text" class="inputbox" name="list_order" size="4" maxlength="4" value="<?php $db->sp("list_order"); ?>" />
       </td>
     </tr>
-    <tr class="row0"> 
+    <tr class="row0">
       <td colspan="2">&nbsp;</td>
     </tr>
   </table>
 <?php
         $tabs->endTab();
         $tabs->startTab( $VM_LANG->_('PHPSHOP_CONFIG'), "config-page");
-        
+
         if( $_PAYMENT->has_configuration() ) {
         	$ps_html->writableIndicator( CLASSPATH."payment/".get_class($_PAYMENT).".cfg.php" );
         }
-        
+
         $_PAYMENT->show_configuration();
-        
+
         echo '<br />
 <strong>'.$VM_LANG->_('VM_PAYMENT_EXTRAINFO').':';
-		echo mm_ToolTip( $VM_LANG->_('VM_PAYMENT_EXTRAINFO_TIP') ) 
+		echo mm_ToolTip( $VM_LANG->_('VM_PAYMENT_EXTRAINFO_TIP') )
 	?>
 <br />
 <textarea class="inputbox" name="payment_extrainfo" cols="120" rows="20"><?php echo htmlspecialchars( $db->sf("payment_extrainfo") ); ?></textarea>
@@ -189,7 +202,7 @@ $funcname = !empty($payment_method_id) ? "paymentMethodUpdate" : "paymentMethodA
 // and finally close the form:
 $formObj->finishForm( $funcname, $modulename.'.payment_method_list', $option );
 ?>
-  
+
   <script type="text/javascript">
 function check() {
    if (document.adminForm.enable_processor[0].checked == true || document.adminForm.enable_processor[1].checked == true) {

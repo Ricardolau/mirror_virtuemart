c5af11e9391dea3127ccec27159ef317d1048103
joomlacorner <joomlacorner@87cc427b-4a1e-0410-b729-b2260ec0f063>
2009-08-31 07:10:47 +0000

    change default  email in paysbuy configuration file.
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@1877 87cc427b-4a1e-0410-b729-b2260ec0f063

--- branches/virtuemart-1.1/virtuemart/classes/payment/ps_paysbuy.cfg.php
+++ branches/virtuemart-1.1/virtuemart/classes/payment/ps_paysbuy.cfg.php
@@ -1,7 +1,7 @@
 <?php
 if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' ); ; 
 
-define ('PaySbuy_EMAIL', 'akarawuth@marvelic.co.th');
+define ('PaySbuy_EMAIL', 'you@yourbusiness.com');
 define ('PaySbuy_CURRENCY', '840');
 define ('PaySbuy_GATEWAY', '1');
 define ('PaySbuy_LANG', '0');

d8da597de56de872c477b9ffb3077aa28259228a
zanardi <zanardi@87cc427b-4a1e-0410-b729-b2260ec0f063>
2010-07-03 12:12:17 +0000

    Version information about 1.1.5 stable release
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@2460 87cc427b-4a1e-0410-b729-b2260ec0f063

--- branches/virtuemart-1.1/virtuemart/version.php
+++ branches/virtuemart-1.1/virtuemart/version.php
@@ -30,22 +30,21 @@ class vmVersion {
 	/** @var string Product */
 	var $PRODUCT = 'VirtueMart';
 	/** @var int Release Number */
-	var $RELEASE = '1.1.4';
+	var $RELEASE = '1.1.5';
 	/** @var string Development Status */
 	var $DEV_STATUS = 'stable';
 	/** @var string Codename */
-	// Song by Milosh (http://www.k7.com/welcome.php?GoTo=iii.I.PRO2112)
-	var $CODENAME = 'Gentle Samui';
+	var $CODENAME = 'Wiesbaden';
 	/** @var string Date */
-	var $RELDATE = '16/10/2009';
+	var $RELDATE = '03/07/2010';
 	/** @var string Time */
-	var $RELTIME = '20:00';
+	var $RELTIME = '12:00';
 	/** @var string Timezone */
 	var $RELTZ = 'GMT';
 	/** @var string Revision */
 	var $REVISION = '$Revision$';
 	/** @var string Copyright Text */
-	var $COPYRIGHT = 'Copyright (C) 2005-2009 VirtueMart Development Team - All rights reserved.'; 
+	var $COPYRIGHT = 'Copyright (C) 2005-2010 VirtueMart Development Team - All rights reserved.'; 
 	/** @var string URL */
 	var $URL = '<a href="http://virtuemart.net">VirtueMart</a> is a Free Component for Joomla!/Mambo released under the GNU/GPL License.';
 }

77c84fb42091083a68de4f2e778e8e9d158eadeb
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2012-11-08 17:28:01 +0000

    fixing rounding prices in category and product details
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@6648 87cc427b-4a1e-0410-b729-b2260ec0f063

--- branches/com_virtuemart.2.0.12f/com_virtuemart.2.0.12f/components/com_virtuemart/views/productdetails/tmpl/default_showprices.php
+++ branches/com_virtuemart.2.0.12f/com_virtuemart.2.0.12f/components/com_virtuemart/views/productdetails/tmpl/default_showprices.php
@@ -20,6 +20,7 @@ defined ('_JEXEC') or die('Restricted access');
 ?>
 <div class="product-price" id="productPrice<?php echo $this->product->virtuemart_product_id ?>">
 	<?php
+	vmdebug('showprices', $this->product->prices);
 	if (!empty($this->product->prices)) {
 		echo "<strong>" . JText::_ ('COM_VIRTUEMART_CART_PRICE') . "</strong>";
 	}
@@ -37,10 +38,10 @@ defined ('_JEXEC') or die('Restricted access');
 
 	}
 	echo $this->currency->createPriceDiv ('variantModification', 'COM_VIRTUEMART_PRODUCT_VARIANT_MOD', $this->product->prices);
-	if (round($this->product->prices['basePriceWithTax'],VmConfig::get('salesPriceRounding')) != $this->product->prices['salesPrice']) {
+	if (round($this->product->prices['basePriceWithTax'],$this->currency->_priceConfig['salesPrice'][1]) != $this->product->prices['salesPrice']) {
 		echo '<span class="price-crossed" >' . $this->currency->createPriceDiv ('basePriceWithTax', 'COM_VIRTUEMART_PRODUCT_BASEPRICE_WITHTAX', $this->product->prices) . "</span>";
 	}
-	if (round($this->product->prices['salesPriceWithDiscount'],VmConfig::get('salesPriceRounding')) != $this->product->prices['salesPrice']) {
+	if (round($this->product->prices['salesPriceWithDiscount'],$this->currency->_priceConfig['salesPrice'][1]) != $this->product->prices['salesPrice']) {
 		echo $this->currency->createPriceDiv ('salesPriceWithDiscount', 'COM_VIRTUEMART_PRODUCT_SALESPRICE_WITH_DISCOUNT', $this->product->prices);
 	}
 	echo $this->currency->createPriceDiv ('salesPrice', 'COM_VIRTUEMART_PRODUCT_SALESPRICE', $this->product->prices);

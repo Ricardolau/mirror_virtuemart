526356b755702a52106aa4cfb4da04f6283d2f52
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2014-12-02 10:17:24 +0000

    klikandpay: sign up
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@8601 87cc427b-4a1e-0410-b729-b2260ec0f063

--- branches/com_virtuemart2.0.14mp/plugins/vmpayment/klikandpay/klikandpay/elements/getklikandpay.php
+++ branches/com_virtuemart2.0.14mp/plugins/vmpayment/klikandpay/klikandpay/elements/getklikandpay.php
@@ -30,62 +30,18 @@ class JElementGetKlikandpay extends JElement {
 
 	function fetchElement ($name, $value, &$node, $control_name) {
 
-		$js = '
-//<![CDATA[
-		jQuery(document).ready(function( $ ) {
-
-		    jQuery("#klikandpay_getklikandpay_link").click( function() {
-				 if ( $("#klikandpay_getklikandpay_show_hide").is(":visible") ) {
-				  $("#klikandpay_getklikandpay_show_hide").hide("slow");
-			        $("#klikandpay_getklikandpay_link").html("' . addslashes (vmText::_ ('VMPAYMENT_KLIKANDPAY_ALREADY_ACCOUNT')) . '");
-				} else {
-				 $("#klikandpay_getklikandpay_show_hide").show("slow");
-			       $("#klikandpay_getklikandpay_link").html("' . addslashes (vmText::_ ('VMPAYMENT_KLIKANDPAY_GET_KLIKANDPAY_HIDE')) . '");
-			    }
-		    });
-		});
-//]]>
-';
-
-		$doc = JFactory::getDocument ();
-		$doc->addScriptDeclaration ($js);
-
-		$cid = vRequest::getvar ('cid', NULL, 'array');
-		if (is_Array ($cid)) {
-			$virtuemart_paymentmethod_id = $cid[0];
+		$lang = $this->getLang();
+		if ($lang == 'fr') {
+			$url = "https://www.klikandpay.com/cgi-bin/connexion.pl?FROM=869B834067";
 		} else {
-			$virtuemart_paymentmethod_id = $cid;
+			$url = "https://www.klikandpay.com/cgi-bin/connexion.pl?FROM=869B834067&L=en";
 		}
+		$logo = '<img src="https://www.klikandpay.com/images/logo_en.png" style="width: 150px;">';
+		$html = '<p><a target="_blank" href="' . $url . '"  >' . $logo . '</a></p>';
 
-		$query = "SELECT * FROM `#__virtuemart_paymentmethods` WHERE  virtuemart_paymentmethod_id = '" . $virtuemart_paymentmethod_id . "'";
-		$db = JFactory::getDBO ();
-		$db->setQuery ($query);
-		$params = $db->loadObject();
+		$html .= '<p><a class="signin-button-link" href="' . $url . '" target="_blank">' . vmText::_('VMPAYMENT_KLIKANDPAY_GET') . '</a>';
+		$html .= ' <a target="_blank" href="https://www.youtube.com/watch?v=DVcUU3FiuMM" class="signin-button-link">' . vmText::_('VMPAYMENT_KLIKANDPAY_DOCUMENTATION') . '</a></p>';
 
-	if ($params->created_on==$params->modified_on ) {
-		$id = "klikandpay_getklikandpay_link";
-		$html = '<a href="#" id="' . $id . '">' . vmText::_ ('VMPAYMENT_KLIKANDPAY_GET_KLIKANDPAY_HIDE') . '</a>';
-		$display='';
-		$html .= '<div id="klikandpay_getklikandpay_show_hide" align=""'.$display.' >';
-	} else {
-		$id = "klikandpay_getklikandpay_link";
-		$html = '<a href="#" id="' . $id . '">' . vmText::_ ('VMPAYMENT_KLIKANDPAY_ALREADY_ACCOUNT') . '</a>';
-		$display=' style="display: none;"';
-		$html .= '<div id="klikandpay_getklikandpay_show_hide" align=""'.$display.' >';
-	}
-		$id="";
-
-
-		$lang = $this->getLang ();
-
-	;
-		if ($lang=='fr') {
-			$url="http://www1.klikandpay.com/default.aspx?0.5187125992961228";
-		} else {
-			$url="http://www1.klikandpay.com/default.aspx?0.6721770374570042";
-		}
-		$html .= '<iframe src="' . $url . '" scrolling="yes" style="x-overflow: none;" frameborder="0" height="1400px" width="800px"></iframe>';
-		$html .= "</div>";
 		return $html;
 	}
 

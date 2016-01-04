554b1b724b57d90c4bb22ff9e7a2cf7f11ae8ed6
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2016-01-04 19:22:05 +0000

    amazon: refund, invalidPaymentMethod, fix with amount saved when order currency <> user curency, amazonRejected cancels payment
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@9100 87cc427b-4a1e-0410-b729-b2260ec0f063

--- branches/com_virtuemart2.0.14mp/plugins/vmpayment/amazon/amazon.php
+++ branches/com_virtuemart2.0.14mp/plugins/vmpayment/amazon/amazon.php
@@ -1,6 +1,5 @@
 <?php
 defined('_JEXEC') or die('Direct Access to ' . basename(__FILE__) . 'is not allowed.');
-
 /**
  *
  * @package    VirtueMart
--- branches/com_virtuemart2.0.14mp/plugins/vmpayment/amazon/helpers/refundnotification.php
+++ branches/com_virtuemart2.0.14mp/plugins/vmpayment/amazon/helpers/refundnotification.php
@@ -1,5 +1,4 @@
 <?php
-
 defined('_JEXEC') or die('Direct Access to ' . basename(__FILE__) . 'is not allowed.');
 
 /**

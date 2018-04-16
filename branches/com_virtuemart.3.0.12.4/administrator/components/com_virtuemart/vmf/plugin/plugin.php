05666819e178a1697169cbd8f876410a7abed800
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2018-04-16 09:46:06 +0000

    For merging branch to trunk, merged parts of the trunk to branch, to merge it then completly to the trunk.
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@9819 87cc427b-4a1e-0410-b729-b2260ec0f063

---
+++ branches/com_virtuemart.3.0.12.4/administrator/components/com_virtuemart/vmf/plugin/plugin.php
@@ -0,0 +1,184 @@
+<?php
+/**
+ * @package     Joomla.Libraries
+ * @subpackage  Plugin
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+if(!class_exists('vPluginHelper')) require(VMPATH_ADMIN .'/vmf/plugin/helper.php');
+if(!class_exists('Registry')) require(VMPATH_ADMIN .'/vmf/registry/Registry.php');
+//use Joomla\Registry\Registry;
+
+/**
+ * JPlugin Class
+ *
+ * @since  1.5
+ */
+abstract class vPlugin
+{
+	/**
+	 * A Registry object holding the parameters for the plugin
+	 *
+	 * @var    Registry
+	 * @since  1.5
+	 */
+	public $params = null;
+
+	/**
+	 * The name of the plugin
+	 *
+	 * @var    string
+	 * @since  1.5
+	 */
+	protected $_name = null;
+
+	/**
+	 * The plugin type
+	 *
+	 * @var    string
+	 * @since  1.5
+	 */
+	protected $_type = null;
+
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  3.1
+	 */
+	protected $autoloadLanguage = false;
+
+	/**
+	 * Constructor
+	 *
+	 * @param   object  &$subject  The object to observe
+	 * @param   array   $config    An optional associative array of configuration settings.
+	 *                             Recognized key values include 'name', 'group', 'params', 'language'
+	 *                             (this list is not meant to be comprehensive).
+	 *
+	 * @since   1.5
+	 */
+	public function __construct(&$subject, $config = array())
+	{
+		// Get the parameters.
+		if (isset($config['params']))
+		{
+			if ($config['params'] instanceof Registry)
+			{
+				$this->params = $config['params'];
+			}
+			else
+			{
+				$this->params = new Registry;
+				$this->params->loadString($config['params']);
+			}
+		}
+
+		// Get the plugin name.
+		if (isset($config['name']))
+		{
+			$this->_name = $config['name'];
+		}
+
+		// Get the plugin type.
+		if (isset($config['type']))
+		{
+			$this->_type = $config['type'];
+		}
+
+		// Load the language files if needed.
+		if ($this->autoloadLanguage)
+		{
+			$this->loadLanguage();
+		}
+
+		if (property_exists($this, 'app'))
+		{
+			$reflection = new ReflectionClass($this);
+			$appProperty = $reflection->getProperty('app');
+
+			if ($appProperty->isPrivate() === false && is_null($this->app))
+			{
+				$this->app = vFactory::getApplication();
+			}
+		}
+
+		if (property_exists($this, 'db'))
+		{
+			$reflection = new ReflectionClass($this);
+			$dbProperty = $reflection->getProperty('db');
+
+			if ($dbProperty->isPrivate() === false && is_null($this->db))
+			{
+				$this->db = vFactory::getDbo();
+			}
+		}
+
+		// Register the observer ($this) so we can be notified
+		$subject->attach($this);
+
+		// Set the subject to observe
+		$this->_subject = &$subject;
+	}
+
+	/**
+	 * Method to trigger events.
+	 * The method first generates the even from the argument array. Then it unsets the argument
+	 * since the argument has no bearing on the event handler.
+	 * If the method exists it is called and returns its return value. If it does not exist it
+	 * returns null.
+	 *
+	 * @param   array  &$args  Arguments
+	 *
+	 * @return  mixed  Routine return value
+	 *
+	 * @since   11.1
+	 */
+	public function update(&$args)
+	{
+		// First let's get the event from the argument array.  Next we will unset the
+		// event argument as it has no bearing on the method to handle the event.
+		$event = $args['event'];
+		unset($args['event']);
+
+		/*
+		 * If the method to handle an event exists, call it and return its return
+		 * value.  If it does not exist, return null.
+		 */
+		if (method_exists($this, $event))
+		{
+			return call_user_func_array(array($this, $event), $args);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Loads the plugin language file
+	 *
+	 * @param   string  $extension  The extension for which a language file should be loaded
+	 * @param   string  $basePath   The basepath to use
+	 *
+	 * @return  boolean  True, if the file has successfully loaded.
+	 *
+	 * @since   1.5
+	 */
+	public function loadLanguage($extension = '', $basePath = VMPATH_ADMINISTRATOR)
+	{
+		if (empty($extension))
+		{
+			$extension = 'Plg_' . $this->_type . '_' . $this->_name;
+		}
+
+		$lang = vFactory::getLanguage();
+
+		return $lang->load(strtolower($extension), $basePath, null, false, true)
+			|| $lang->load(strtolower($extension), VMPATH_PLUGINS . '/' . $this->_type . '/' . $this->_name, null, false, true);
+	}
+}

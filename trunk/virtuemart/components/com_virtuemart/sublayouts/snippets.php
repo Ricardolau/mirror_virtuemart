58eb31e17fb50f3a154d029bb19a185ca711d98a
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2019-07-08 09:34:26 +0000

    - Update of the snippets.php for Searchengines
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@10092 87cc427b-4a1e-0410-b729-b2260ec0f063

--- trunk/virtuemart/components/com_virtuemart/sublayouts/snippets.php
+++ trunk/virtuemart/components/com_virtuemart/sublayouts/snippets.php
@@ -1,6 +1,7 @@
 <?php
 // Check to ensure this file is included in Joomla!
 defined('_JEXEC') or die('Restricted access');
+$document = JFactory::getDocument ();
 $product = $viewData['product'];
 $currency = $viewData['currency'];
 $view = vRequest::getCmd('view');
@@ -12,53 +13,127 @@ if (is_object($productrating)) {
 	$productratingcount = '0';
 }
 
+$canonicalUrl = JUri::getInstance()->toString(array('scheme', 'host', 'port')).JRoute::_($product->canonical);
+$name = htmlspecialchars(strip_tags($product->product_name));
+if (VmConfig::get('show_manufacturers', 1)){
+	$brand = (!empty($product->mf_name)? $product->mf_name : '');
+} else {
+	$brand = '';
+}
+$sku =  (!empty($product->product_sku)? $product->product_sku : '');
+$productID =  $sku;
+$mpn =  (!empty($product->product_mpn)? $product->product_mpn : '');
+$review =  (!empty($ratingModel->ids[0]->comment)? $ratingModel->ids[0]->comment : '');
+$author =  (!empty($ratingModel->ids[0]->name)? $ratingModel->ids[0]->name : '');
+
+//do something sensible and not fixed with valid price date
+$DownDate = new DateTime($product->prices["product_price_publish_down"]);
+$DownCalcDate = new DateTime();
+if ($DownDate > $DownCalcDate) {
+	$priceUntil = $DownDate->format( "Y-m-d" );
+} else {
+//  $DownCalcDate->modify('+20 years');
+//  $priceUntil = $DownCalcDate->format( "Y-m-d" );
+//  dont show an end to this price if there is not one
+	$priceUntil = '';
+}
+
 //GJC look for parent & for child stock
-if($product->product_parent_id == 0) {
+$stockog = 'OutofStock';
+$stockhandle = VmConfig::get ('stockhandle', 'none');
+$finalstk = 0;
+if ($stockhandle == 'disableit' or $stockhandle == 'disableadd' or $stockhandle == 'none') {
+	$stockog = 'InStock';
+} elseif ($product->product_parent_id == 0) {
 	$prodmodel = VmModel::getModel ('product');
 	$children = $prodmodel->getProductChilds($product->virtuemart_product_id);
 	$finalstk = '0';
 	foreach($children as $child){
 		$finalstk += $child->product_in_stock;
 	}
-}else{
+} else {
 	$finalstk = $product->product_in_stock;
 }
-$stockog = 'OutofStock';
-if($finalstk > 0){
+if ($finalstk > 0) {
 	$stockog = 'InStock';
 }
+
+//check for meta if empty move onto using product data
+if (!empty($document->getMetaData('description'))) {
+	$description = $document->getMetaData('description');
+}
+if (empty($description)){
+	if (!empty($product->product_s_desc)){
+		$description = htmlspecialchars(strip_tags($product->product_s_desc));
+	} else {
+		$description = htmlspecialchars(strip_tags($product->product_desc));
+	}
+}
+
 ?>
 
 <script type="application/ld+json">
 {
   "@context": "http://schema.org/",
   "@type": "Product",
-  "name": "<?php echo $product->product_name; ?>",
-
-	<?php /*"brand" :"echo $brand;",*/ ?>
-	"productID" :"<?php echo $product->product_sku; ?>",
-	"sku" :"<?php echo $product->product_sku; ?>",
-  <?php if ( $product->images[0]->virtuemart_media_id > 0) { ?>
-  "image": "<?php echo JURI::root().$product->images[0]->file_url; ?>",
-  <?php } ?>
-  <?php if (!empty($product->product_s_desc)) { ?>
-  "description": "<?php echo strip_tags($product->product_s_desc); ?>",
-  <?php } elseif (!empty($product->product_desc)) { ?>
-  "description": "<?php echo strip_tags($product->product_desc); ?>",
-  <?php } ?>
-  <?php if ($viewData['showRating']) { ?>
+  "name": "<?php echo $name; ?>",
+  "description":"<?php echo $description; ?>",
+<?php if ($brand) { ?>
+  "brand": {
+    "@type": "Thing",
+    "name": "<?php echo $brand; ?>"
+  },
+<?php
+	}?>
+<?php if ($productID) { ?>
+  "productID":"<?php echo $productID; ?>",
+<?php } ?>
+<?php if ($sku) { ?>
+  "sku": "<?php echo $sku; ?>",
+<?php } ?>
+<?php if ($mpn) { ?>
+  "mpn": "<?php echo $mpn; ?>",
+<?php } ?>
+<?php if ( $product->images[0]->virtuemart_media_id > 0) {
+		$i = 0;
+		$numimages = count($product->images); ?>
+  "image": [
+<?php
+		foreach($product->images as $image){
+			echo '    "' . JURI::root().$image->file_url. '"';
+			if (++$i != $numimages){
+				echo ', ';
+			}
+		} ?>
+  ],
+<?php  } ?>
+<?php if ($viewData['showRating'] && $productratingcount > 0) { ?>
   "aggregateRating":{
     "@type": "AggregateRating",
-    "ratingValue": "<?php if (!empty($product->rating)) { echo $product->rating;} else { echo '0'; } ?>",
+    "ratingValue": "<?php echo $product->rating; ?>",
     "reviewCount": "<?php echo $productratingcount; ?>"
   },
-  <?php } ?>
-  "offers":{
+<?php } ?>
+<?php if ($review) { ?>
+  "review": {
+    "@type": "Review",
+    "reviewBody": "<?php echo $review; ?>",
+    "author": {
+      "@type": "Person",
+      "name": "<?php echo $author; ?>"
+    }
+  },
+<?php } ?>
+  "offers": {
     "@type": "Offer",
     "priceCurrency": "<?php echo $currency->_vendorCurrency_code_3; ?>",
-	"availability" :"<?php echo $stockog; ?>",
+    "availability": "<?php echo $stockog; ?>",
     "price": "<?php echo $product->prices['salesPrice']; ?>",
-	 "url" : "<?php  echo JUri::getInstance(); ?>"
+    "url": "<?php  echo $canonicalUrl; ?>",
+<?php if ($priceUntil) { ?>
+    "priceValidUntil": "<?php  echo $priceUntil; ?>",
+<?php } ?>
+    "itemCondition": "NewCondition"
   }
 }
 </script>

db6998151b572001f498e8fbe0099e12ee6146ce
StefanSTS <StefanSTS@87cc427b-4a1e-0410-b729-b2260ec0f063>
2019-10-09 04:59:02 +0000

    Added copying of lang files into the plugin folder on installation. Added working lang files en-GB. Button layout in plugin settings to Joomla standard.
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@10162 87cc427b-4a1e-0410-b729-b2260ec0f063

--- trunk/virtuemart/plugins/system/vmLoaderPluginUpdate/vmLoaderPluginUpdate.php
+++ trunk/virtuemart/plugins/system/vmLoaderPluginUpdate/vmLoaderPluginUpdate.php
@@ -4,7 +4,7 @@
  * Version 1.0, 2017-08-06
  * @author Reinhold Kainhofer, Open Tools
  * @copyright Copyright (C) 2017 Reinhold Kainhofer - All rights reserved.
- * @license - http://www.gnu.org/licenses/gpl.html GNU/GPL 
+ * @license - http://www.gnu.org/licenses/gpl.html GNU/GPL
 
  * The Joomla 3.7+ plugin updater tries to load the plugin before
  * updating. Since many plugins in the past assumed that they could only
@@ -21,12 +21,14 @@ jimport('joomla.event.plugin');
 
 class plgSystemVmLoaderPluginUpdate extends JPlugin {
 
+	protected $autoloadLanguage = true;
+
 	function __construct(&$subject, $config = array()) {
 		parent::__construct($subject, $config);
 		# Only for Joomla 3.7 and above:
 		# If a plugin update is run (option=com_installer&view=update&task=update.update),
 		# then load the VM config. This approach will load VM even for non-VM
-		# plugins, but the overhead of checking whether the updated plugin 
+		# plugins, but the overhead of checking whether the updated plugin
 		# really requires VM is way too large compared to the performance hit
 		# of loading VM even for non-VM plugins.
 		if(version_compare(JVERSION,'3.7.0','ge')) {

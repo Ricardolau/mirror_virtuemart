e6c2ce612a7e3eb48b47bc75626904cb213cf0d0
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2020-11-02 18:30:05 +0000

    iStraxx added the toggleCartButton.js with MIT license, need for the textinput required letters. ajax_catree.js has now value 0 to unset any category
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@10370 87cc427b-4a1e-0410-b729-b2260ec0f063

--- trunk/virtuemart/components/com_virtuemart/assets/js/ajax_catree.js
+++ trunk/virtuemart/components/com_virtuemart/assets/js/ajax_catree.js
@@ -4,7 +4,7 @@
  * @package	VirtueMart
  * @subpackage Javascript Library
  * @author Max Milbers
- * @copyright Copyright (c) 2016 VirtueMart Team. All rights reserved.
+ * @copyright Copyright (c) 2020 VirtueMart Team. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
 // vmText::sprintf( 'COM_VIRTUEMART_SELECT' ,  vmText::_('COM_VIRTUEMART_CATEGORY'))
@@ -50,7 +50,7 @@ Virtuemart.loadCategoryTree = function(id){
 		data: Virtuemart.ajaxCategoryUrl,
 		success:function(json){
 			jQuery('select#'+id).switchClass('chzn-done','chzn-select');
-			jQuery('select#'+id).html('<option value=\"\">'+Virtuemart.emptyCatOpt+'</option>'+json.value);
+			jQuery('select#'+id).html('<option value=\"0\">'+Virtuemart.emptyCatOpt+'</option>'+json.value);
 			jQuery('[data-vm="ajax_cat_load"]').remove();
 			jQuery('select#'+id).chosen();
 			Virtuemart.stopVmLoading();

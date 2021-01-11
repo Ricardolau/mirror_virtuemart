251c854cc08b9c79225783f50e0d1575194694eb
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2021-01-11 11:23:39 +0000

    New admin template Config views updating Id property
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@10393 87cc427b-4a1e-0410-b729-b2260ec0f063

--- trunk/virtuemart/administrator/templates/vmadmin/html/com_virtuemart/config/default_email_invoices.php
+++ trunk/virtuemart/administrator/templates/vmadmin/html/com_virtuemart/config/default_email_invoices.php
@@ -6,13 +6,13 @@
  * @subpackage Config
  * @author Oscar van Eijk
  * @link https://virtuemart.net
- * @copyright Copyright (c) 2015 VirtueMart Team. All rights reserved.
+ * @copyright Copyright (c) 2015 - ${PHING.VM.COPYRIGHT} VirtueMart Team. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  * VirtueMart is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
- * @version $Id: default_checkout.php 9008 2015-10-04 20:41:08Z Milbo $
+ * @version $Id$
  */
 defined('_JEXEC') or die('Restricted access');
 $attrlist = 'class="inputbox" multiple="multiple" ';
--- trunk/virtuemart/administrator/templates/vmadmin/html/com_virtuemart/config/default_shopfront_product.php
+++ trunk/virtuemart/administrator/templates/vmadmin/html/com_virtuemart/config/default_shopfront_product.php
@@ -7,13 +7,13 @@
  * @subpackage Config
  * @author RickG
  * @link https://virtuemart.net
- * @copyright Copyright (c) 2004 - 2010 VirtueMart Team. All rights reserved.
+ * @copyright Copyright (c) 2004 - ${PHING.VM.COPYRIGHT} VirtueMart Team. All rights reserved.
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
  * VirtueMart is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
- * @version $Id: default_shopfront.php 10312 2020-05-04 13:14:45Z Milbo $
+ * @version $Id$
  */
 
 // Check to ensure this file is included in Joomla!

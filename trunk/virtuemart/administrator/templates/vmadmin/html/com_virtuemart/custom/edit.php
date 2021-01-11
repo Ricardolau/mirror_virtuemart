863437cec60ba3efb455f158d66b45a4311e1e1d
alatak <alatak@87cc427b-4a1e-0410-b729-b2260ec0f063>
2021-01-11 13:13:14 +0000

    New admin template views icon renamed
    git-svn-id: https://dev.virtuemart.net/svn/virtuemart@10403 87cc427b-4a1e-0410-b729-b2260ec0f063

--- trunk/virtuemart/administrator/templates/vmadmin/html/com_virtuemart/custom/edit.php
+++ trunk/virtuemart/administrator/templates/vmadmin/html/com_virtuemart/custom/edit.php
@@ -30,7 +30,7 @@ vmJsApi::JvalideForm();
 		<div class="uk-card-header">
 			<div class="uk-card-title">
 						<span class="md-color-cyan-600 uk-margin-small-right"
-								uk-icon="icon: magic-wand; ratio: 1.2"></span>
+								uk-icon="icon: customfield; ratio: 1.2"></span>
 				<?php echo vmText::_('COM_VIRTUEMART_PRODUCT_CUSTOM_FIELD') ?>
 			</div>
 		</div>
